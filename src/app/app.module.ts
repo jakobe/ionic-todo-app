9bddab0a740e59345020f6f0bd1f7104b4827090
Jakob Engelbrecht Olesen <jakob@basher.dk>
2017-11-19 11:34:12 +0100

    Removed statusbar and splashcsreen

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { ErrorHandler, NgModule } from '@angular/core';
 import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
-import { SplashScreen } from '@ionic-native/splash-screen';
-import { StatusBar } from '@ionic-native/status-bar';
 import { IonicStorageModule } from '@ionic/storage';
 
 import { MyApp } from './app.component';
@@ -31,8 +29,6 @@ import { DataProvider } from '../providers/data/data';
     TodayPage
   ],
   providers: [
-    StatusBar,
-    SplashScreen,
     DataProvider,
     {provide: ErrorHandler, useClass: IonicErrorHandler}
   ]
