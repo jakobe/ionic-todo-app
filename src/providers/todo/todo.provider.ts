1dd84cfd58c011a3a95161094ea21de57975737e
Jakob Engelbrecht Olesen <jakob@basher.dk>
2017-11-25 19:36:44 +0100

    Added storage to TodoProvider

--- src/providers/todo/todo.provider.ts
+++ src/providers/todo/todo.provider.ts
@@ -1,10 +1,16 @@
 import { Injectable } from '@angular/core';
+import { Storage } from '@ionic/storage';
 import { BehaviorSubject } from 'rxjs/BehaviorSubject';
 import 'rxjs/add/operator/map';
 import 'rxjs/add/operator/filter';
 
 import { TodoItem } from '../../models/TodoItem';
 
+const STORAGE_KEY = 'todoItems';
+
+function _todoItemFromData(data) {
+  return new TodoItem(data.title, data.description, data.isDone, data.dueDate, data.dueTonight, data.call);
+}
 /*
   Generated class for the TodoProvider provider.
 
@@ -17,31 +23,51 @@ export class TodoProvider {
   private dataStore: {
     todos: TodoItem[]
   };
-  constructor() {
+  constructor(private storage: Storage) {
     this._todos = <BehaviorSubject<TodoItem[]>>new BehaviorSubject(undefined);
+    this.dataStore =  {
+      todos: []
+    }
+    this._todos.next(Object.assign({}, this.dataStore).todos);
     this.loadInitialData();
   }
 
-  loadInitialData() {
-    var yesterday = new Date();
-    yesterday.setDate(yesterday.getDate() -1);
-    this.dataStore = { todos: [
-      new TodoItem ('Arrange music for party', 'DJ Trump', false, yesterday, true, true),
-      new TodoItem ('Buy cake for mom', 'The Blueberry one she loves so much'),
-      new TodoItem ('Try on new shoes', 'Those fancy ones from Adidas', false, TodoItem.today),
-      new TodoItem ('Buy something', 'Maybe something nice...', true),
-    ] };
-    this._todos.next(Object.assign({}, this.dataStore).todos);    
+ loadInitialData() {
+    this.loadFromStorage().then(result => {
+      if(result) {
+        this.dataStore.todos = result.map(_todoItemFromData);
+      } else {
+        var yesterday = new Date();
+        yesterday.setDate(yesterday.getDate() -1);
+        this.dataStore.todos = [
+          new TodoItem ('Arrange music for party', 'DJ Trump', false, yesterday, true, true),
+          new TodoItem ('Buy cake for mom', 'The Blueberry one she loves so much'),
+          new TodoItem ('Try on new shoes', 'Those fancy ones from Adidas', false, TodoItem.today),
+          new TodoItem ('Buy something', 'Maybe something nice...', true),
+        ];
+        this.saveToStorage();
+      }
+      this._todos.next(Object.assign({}, this.dataStore).todos);   
+    });
+  }
+
+  private loadFromStorage() {
+    return this.storage.get(STORAGE_KEY);
+  }
+
+  private saveToStorage() {
+    return this.storage.set(STORAGE_KEY, this.dataStore.todos);    
   }
 
   save() {
+    this.saveToStorage();
     this._todos.next(Object.assign({}, this.dataStore).todos);    
   }
 
   create(todo: TodoItem) {
     //this.http.post(`${this.baseUrl}/todos`, JSON.stringify(todo)).subscribe(data => {
         this.dataStore.todos.unshift(todo);
-        this._todos.next(Object.assign({}, this.dataStore).todos);
+        this.save();       
       //}, error => console.log('Could not create todo.'));
   }
 
