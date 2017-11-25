bd00a10d27ccd912365fbff78f5b888a74b5c79b
Jakob Engelbrecht Olesen <jakob@basher.dk>
2017-11-25 16:04:13 +0100

    Added dueTonight, isOverdue, isDueToday properties + static today property

--- src/models/TodoItem.ts
+++ src/models/TodoItem.ts
@@ -1,8 +1,32 @@
 export class TodoItem {
-    constructor(
-      public title: string,
-      public description: string,
-      public isDone: boolean = false,
-      public dueDate: Date = null,
-      public call: boolean = false) { }
+  
+  List: any;
+
+  constructor(
+    public title: string,
+    public description: string,
+    public isDone: boolean = false,
+    public dueDate: Date = null,
+    public dueTonight: boolean = false,
+    public call: boolean = false)
+    {
+    }
+
+  public get isOverdue():boolean {
+    return this.dueDate && TodoItem.today > this.dueDate;
+  };
+
+  public get isDueToday():boolean {
+    var today = TodoItem.today;
+    return this.dueDate &&
+            this.dueDate.getFullYear() === today.getFullYear() &&
+            this.dueDate.getMonth() === today.getMonth() &&
+            this.dueDate.getDate() === today.getDate();
+  };
+
+  static get today() : Date {
+    var today = new Date();
+    today.setHours(0, 0, 0, 0);
+    return today;   
   }
+}
